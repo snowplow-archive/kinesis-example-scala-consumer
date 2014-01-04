41e81bd58fb360ce0e298fca97f22b52d3abc2ce
Brandon Amos <bdamos@vt.edu>
2014-01-04 12:50:24 -0500

    Translate the entry point to Scala.

--- src/main/scala/com.snowplowanalytics.kinesis.consumer/KinesisConsumer.scala
+++ src/main/scala/com.snowplowanalytics.kinesis.consumer/KinesisConsumer.scala
@@ -13,156 +13,71 @@
  * permissions and limitations under the License.
  */
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.util.Properties;
-import java.util.UUID;
+import java.io.{FileInputStream,IOException}
+import java.net.InetAddress
+import java.util.{Properties,UUID}
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorFactory;
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream;
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration;
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.Worker;
-import com.amazonaws.services.kinesis.metrics.impl.NullMetricsFactory;
+import com.amazonaws.AmazonClientException
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider
+import com.amazonaws.auth.InstanceProfileCredentialsProvider
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorFactory
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.{
+  InitialPositionInStream,
+  KinesisClientLibConfiguration,
+  Worker
+}
+import com.amazonaws.services.kinesis.metrics.impl.NullMetricsFactory
 
 
-/**
- * Sample Kinesis Application.
- */
-public final class SampleKinesisApplication {
-    private static final String DEFAULT_APP_NAME = "SnowplowExampleConsumer";
-    private static final String DEFAULT_STREAM_NAME = "snowplow_thrift";
+object KinesisConsumer extends App {
+  // TODO: Add these to config.
+  val DEFAULT_APP_NAME = "SnowplowExampleConsumer"
+  val DEFAULT_STREAM_NAME = "snowplow_thrift"
+  val DEFAULT_KINESIS_ENDPOINT = "https://kinesis.us-east-1.amazonaws.com"
     
-    private static final String DEFAULT_KINESIS_ENDPOINT = "https://kinesis.us-east-1.amazonaws.com";
+  // Initial position in the stream when the application starts.
+  // LATEST: most recent data.
+  // TRIM_HORIZON: oldest available data.
+  val DEFAULT_INITIAL_POSITION = InitialPositionInStream.TRIM_HORIZON
     
-    // Initial position in the stream when the application starts up for the first time.
-    // Position can be one of LATEST (most recent data) or TRIM_HORIZON (oldest available data)
-    private static final InitialPositionInStream DEFAULT_INITIAL_POSITION = InitialPositionInStream.TRIM_HORIZON;
+  val applicationName = DEFAULT_APP_NAME
+  val streamName = DEFAULT_STREAM_NAME
+  val kinesisEndpoint = DEFAULT_KINESIS_ENDPOINT
+  val initialPositionInStream = DEFAULT_INITIAL_POSITION
     
-    private static String applicationName = DEFAULT_APP_NAME;
-    private static String streamName = DEFAULT_STREAM_NAME;
-    private static String kinesisEndpoint = DEFAULT_KINESIS_ENDPOINT;
-    private static InitialPositionInStream initialPositionInStream = DEFAULT_INITIAL_POSITION;
+  var kinesisClientLibConfiguration: KinesisClientLibConfiguration = null
     
-    private static KinesisClientLibConfiguration kinesisClientLibConfiguration;
-    
-    private static final Log LOG = LogFactory.getLog(SampleKinesisApplication.class);
+  // TODO: Replace this with AWS auth. from config.
+  configure
+  
+  println("Starting " + applicationName)
+  println("Running " + applicationName + " to process stream " + streamName)
+  
+  val recordProcessorFactory = new SampleRecordProcessorFactory()
+  val worker = new Worker(
+    recordProcessorFactory,
+    kinesisClientLibConfiguration,
+    new NullMetricsFactory()
+  )
 
-    /**
-     * 
-     */
-    private SampleKinesisApplication() {
-        super();
-    }
-    
-    /**
-     * @param args Property file with config overrides (e.g. application name, stream name)
-     * @throws IOException Thrown if we can't read properties from the specified properties file
-     */
-    public static void main(String[] args) throws IOException {
-        String propertiesFile = null;
-        
-        if (args.length > 1) {
-            System.err.println("Usage: java " + SampleKinesisApplication.class.getName() + " <propertiesFile>");
-            System.exit(1);
-        } else if (args.length == 1) {
-            propertiesFile = args[0];
-        }
+  worker.run()
 
-        configure(propertiesFile);
-        
-        System.out.println("Starting " + applicationName);
-        LOG.info("Running " + applicationName + " to process stream " + streamName);
-        
-        
-        IRecordProcessorFactory recordProcessorFactory = new SampleRecordProcessorFactory();
-         Worker worker = new Worker(recordProcessorFactory, kinesisClientLibConfiguration, new NullMetricsFactory());
+  // TODO: Replace this with AWS auth. from config.
+  def configure = {
+    val workerId = InetAddress.getLocalHost().getCanonicalHostName() +
+      ":" + UUID.randomUUID()
+    println("Using workerId: " + workerId)
 
-        int exitCode = 0;
-        try {
-            worker.run();
-        } catch (Throwable t) {
-            LOG.error("Caught throwable while processing data.", t);
-            exitCode = 1;
-        }
-        System.exit(exitCode);
-    }
-    
-    private static void configure(String propertiesFile) throws IOException {
-        
-        
-        if (propertiesFile != null) {
-            loadProperties(propertiesFile);
-        }
-        
-        // ensure the JVM will refresh the cached IP values of AWS resources (e.g. service endpoints).
-        java.security.Security.setProperty("networkaddress.cache.ttl" , "60");
-        
-        String workerId = InetAddress.getLocalHost().getCanonicalHostName() + ":" + UUID.randomUUID();
-        LOG.info("Using workerId: " + workerId);
-       
-        // Get credentials from IMDS. If unsuccessful, get them from the classpath. 
-       AWSCredentialsProvider credentialsProvider = null;
-        try {
-            credentialsProvider = new InstanceProfileCredentialsProvider();
-            // Verify we can fetch credentials from the provider
-            credentialsProvider.getCredentials();
-            LOG.info("Obtained credentials from the IMDS.");
-        } catch (AmazonClientException e) {
-            LOG.info("Unable to obtain credentials from the IMDS, trying classpath properties", e);
-            credentialsProvider = new ClasspathPropertiesFileCredentialsProvider();
-            // Verify we can fetch credentials from the provider
-            credentialsProvider.getCredentials();
-            LOG.info("Obtained credentials from the properties file.");
-        }
-        
-        LOG.info("Using credentials with access key id: " + credentialsProvider.getCredentials().getAWSAccessKeyId());
-        
-        kinesisClientLibConfiguration = new KinesisClientLibConfiguration(applicationName, streamName, 
-        		credentialsProvider, workerId).withInitialPositionInStream(initialPositionInStream); 
-    }
-
-    /**
-     * @param propertiesFile
-     * @throws IOException Thrown when we run into issues reading properties
-     */
-    private static void loadProperties(String propertiesFile) throws IOException {
-        FileInputStream inputStream = new FileInputStream(propertiesFile);
-        Properties properties = new Properties();
-        try {
-            properties.load(inputStream);
-        } finally {
-            inputStream.close();
-        }
-        
-        String appNameOverride = properties.getProperty(ConfigKeys.APPLICATION_NAME_KEY);
-        if (appNameOverride != null) {
-            applicationName = appNameOverride;
-        }
-        LOG.info("Using application name " + applicationName);
-        
-        String streamNameOverride = properties.getProperty(ConfigKeys.STREAM_NAME_KEY);
-        if (streamNameOverride != null) {
-            streamName = streamNameOverride;
-        }
-        LOG.info("Using stream name " + streamName);
-        
-        String kinesisEndpointOverride = properties.getProperty(ConfigKeys.KINESIS_ENDPOINT_KEY);
-        if (kinesisEndpointOverride != null) {
-            kinesisEndpoint = kinesisEndpointOverride;
-        }
-        String initialPositionOverride = properties.getProperty(ConfigKeys.INITIAL_POSITION_IN_STREAM_KEY);
-        if (initialPositionOverride != null) {
-             initialPositionInStream = InitialPositionInStream.valueOf(initialPositionOverride);
-        }
-         LOG.info("Using initial position " + initialPositionInStream.toString() + " (if a checkpoint is not found).");
-    }
-
+    // TODO.
+    val credentialsProvider = new ClasspathPropertiesFileCredentialsProvider()
+    println("Using credentials with access key id: " +
+      credentialsProvider.getCredentials().getAWSAccessKeyId())
+    kinesisClientLibConfiguration = new KinesisClientLibConfiguration(
+      applicationName,
+      streamName, 
+      credentialsProvider,
+      workerId
+    ).withInitialPositionInStream(initialPositionInStream)
+  }
 }
